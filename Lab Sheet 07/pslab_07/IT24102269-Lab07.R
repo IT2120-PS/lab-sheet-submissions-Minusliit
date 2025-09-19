449794ddf9de3268ce00e3e0e7f57ce534ad916f
IT24102269 Senesh K.H.M <it24102269@my.sliit.lk>
2025-09-19 16:22:17 +0530

    Add files via upload

---
+++ Lab Sheet 07/pslab_07/IT24102269-Lab07.R
@@ -0,0 +1,11 @@
+setwd("C:\\Users\\User\\OneDrive - Sri Lanka Institute of Information Technology\\SLIIT\\Year 2 sem 1\\Probability and Statistics\\week07")
+#Q1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+#Q2
+pexp(2, rate = 1/3)
+#Q3
+#i.
+1 - pnorm(130, mean = 100, sd = 15)
+#ii.
+qnorm(0.95, mean = 100, sd = 15)
+
